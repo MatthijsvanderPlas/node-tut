0a4408e1d1c35315bac7b284efbbd49f6499dd35
matthijsvanderplas <mvdplas84@ziggo.nl>
2023-02-02 15:34:19 +0100

    Initial commit

---
+++ 1-intro.js
@@ -0,0 +1,11 @@
+const amount = 8
+
+if (amount < 10) {
+  console.log("It is a small number")
+} else {
+  console.log("It is a big number")
+}
+
+
+console.log(`Hello World!`)
+console.log(`Hey it is my first node app!!!`)
