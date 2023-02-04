001a8f3c9d775d7ccc22b14f06e7e0e4e01c32e2
matthijsvanderplas <mvdplas84@ziggo.nl>
2023-02-04 09:40:31 +0100

    Initial commit

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,26 @@
-console.log('Express Tutorial')
+const express = require('express')
+const logger = require('./logger')
+
+const app = express()
+
+app.use(logger)
+
+app.get('/', (req, res) => {
+  res.send('Home')
+})
+
+app.get('/about', (req, res) => {
+  res.send('About')
+})
+
+app.get('/api/products', (req, res) => {
+  res.send('Products')
+})
+
+app.get('/api/items', (req, res) => {
+  res.send('Items')
+})
+
+app.listen(5000, () => {
+  console.log(`Running on port 5000...`)
+})
